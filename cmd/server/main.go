d5f2da5862f609b8c85a177718aae2dfce5c9797
Ilia Oberemok <i.oberemok777@gmail.com>
2026-04-29 15:51:57 +0300

    feat: the server will now listen for commands and respond to: pause, resume, and quit

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,13 +5,17 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
+	// this will display the commands the user of the REPL can use
+	gamelogic.PrintServerHelp()
 	connectionString := "amqp://guest:guest@localhost:5672/"
+
 	amqpConnection, err := amqp.Dial(connectionString)
 	if err != nil {
 		log.Fatalf("failed to connect to rabbitmq: %v", err)
@@ -25,13 +29,39 @@ func main() {
 		log.Fatalf("failed to create a channel over the connection: %v", err)
 	}
 
-	pubsub.PublishJSON(amqpChannel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
-		IsPaused: true,
-	})
+	for {
+		words := gamelogic.GetInput()
+		if len(words) == 0 {
+			continue
+		}
 
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan
+		firstWord := words[0]
+		if firstWord == "pause" {
+			log.Print("sending a pause message to the exchange...")
 
+			pubsub.PublishJSON(amqpChannel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+				IsPaused: true,
+			})
+
+			log.Print("pause message published with no problem")
+		} else if firstWord == "resume" {
+			log.Print("sending a resume message to the exchange...")
+
+			pubsub.PublishJSON(amqpChannel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+				IsPaused: false,
+			})
+
+			log.Print("resume message published with no problem")
+		} else if firstWord == "quit" {
+			log.Print("exiting the application...")
+			break
+		} else {
+			log.Print("unknown command")
+		}
+
+		signalChan := make(chan os.Signal, 1)
+		signal.Notify(signalChan, os.Interrupt)
+		<-signalChan
+	}
 	log.Print("shutting down...")
 }
