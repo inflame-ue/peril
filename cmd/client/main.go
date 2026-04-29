468b07ac4d0136e1c11f2f3b139da5fe8c01470b
Ilia Oberemok <i.oberemok777@gmail.com>
2026-04-29 15:39:01 +0300

    feat: implement a test a client in client/main.go that declares and bind a transient queue with no other effect for now.

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"log"
+	"os"
+	"os/signal"
+	"strings"
 
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
@@ -18,5 +24,10 @@ func main() {
 		log.Fatalf("failed to fetch the username: %v", err)
 	}
 
+	queueName := strings.Join([]string{routing.PauseKey, username}, ".")
+	pubsub.DeclareAndBind(amqpConnection, routing.ExchangePerilDirect, queueName, routing.PauseKey, pubsub.Transient)
 	
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
 }
